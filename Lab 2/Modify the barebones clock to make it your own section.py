d8d37563a821bc3910ab41fde8d5d53594e11390
LaboriouslyExquisite <156468486+LaboriouslyExquisite@users.noreply.github.com>
2026-09-20 23:13:51 -0400

    Refactor code by removing redundant comments
    Removed unnecessary comments and organized code sections for clarity.

--- Lab 2/Modify the barebones clock to make it your own section.py
+++ Lab 2/Modify the barebones clock to make it your own section.py
@@ -14,9 +14,9 @@ import adafruit_rgb_display.ssd1351 as ssd1351
 import adafruit_rgb_display.ssd1331 as ssd1331
 
 
-# --------------------------------------------------
+# ---
 # DISPLAY SETUP
-# --------------------------------------------------
+# ---
 
 # Configuration for CS and DC pins
 cs_pin = digitalio.DigitalInOut(board.D5)
@@ -43,9 +43,9 @@ disp = st7789.ST7789(
 )
 
 
-# --------------------------------------------------
+# ---
 # SCREEN SIZE
-# --------------------------------------------------
+# ---
 
 if disp.rotation % 180 == 90:
     height = disp.width
@@ -55,9 +55,9 @@ else:
     height = disp.height
 
 
-# --------------------------------------------------
+# ---
 # CLEAR SCREEN
-# --------------------------------------------------
+# ---
 
 image = Image.new("RGB", (width, height))
 draw = ImageDraw.Draw(image)
@@ -71,28 +71,27 @@ draw.rectangle(
 disp.image(image)
 
 
-# --------------------------------------------------
+# ---
 # BACKLIGHT
-# --------------------------------------------------
+# ---
 
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
 
-# --------------------------------------------------
+# ---
 # BUTTON SETUP
-# --------------------------------------------------
+# ---
 
-# Change D23 if your button uses a different GPIO pin
 button = digitalio.DigitalInOut(board.D23)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
 
-# --------------------------------------------------
+# ---
 # IMAGES
-# --------------------------------------------------
+# ---
 
 # Put your image filenames here
 images = [
@@ -103,9 +102,9 @@ images = [
 current_image = 0
 
 
-# --------------------------------------------------
+# ---
 # FUNCTION TO DISPLAY AN IMAGE
-# --------------------------------------------------
+# ---
 
 def display_image(filename):
 
@@ -143,16 +142,16 @@ def display_image(filename):
     disp.image(image)
 
 
-# --------------------------------------------------
+# ---
 # SHOW FIRST IMAGE
-# --------------------------------------------------
+# ---
 
 display_image(images[current_image])
 
 
-# --------------------------------------------------
+# ---
 # MAIN LOOP
-# --------------------------------------------------
+# ---
 
 while True:
 
